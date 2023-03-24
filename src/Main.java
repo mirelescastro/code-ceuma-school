4e6b76651fc7e6084ece2eedd7c2592a60977399
anaforttes <carolfortes05@gmail.com>
2023-03-24 13:52:54 -0300

    cadastro feito, qualquer atualização irei dar o commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    private static ArrayList<String> alunos = new ArrayList<String>();
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        //Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+
+            String nome = JOptionPane.showInputDialog("Digite o nome do aluno ou 'sair' para encerrar:");
+
+            if (nome.equals("sair")) {
+                continuar = false;
+            } else {
+                cadastrarAluno(nome);
+            }
+        }
+
+        System.out.println("Alunos cadastrados:");
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+        //scanner.close();
+    }
+
+    public static void cadastrarAluno(String nome) {
+        alunos.add(nome);
+        System.out.println("Aluno " + nome + " cadastrado com sucesso!");
     }
 }
