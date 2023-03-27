1290d153f9cf4f09acce31a219dbb53d3a10e1af
dudscamily <eduardacamily@gmail.com>
2023-03-27 17:34:24 -0300

    ...

--- src/Escola.java
+++ src/Escola.java
@@ -1,2 +1,29 @@
 public class Escola {
+
+    public String usuario;
+    public String email;
+    public String id;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,100 @@
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    private static ArrayList<String> alunos = new ArrayList<String>();
+//    private static ArrayList<String> alunos = new ArrayList<String>();
+//    private static Pattern patternEmail = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+//
+//    public static void main(String[] args) {
+//
+//        //Scanner scanner = new Scanner(System.in);
+//        boolean continuar = true;
+//
+//        while (continuar) {
+//
+//            String nome = JOptionPane.showInputDialog("Digite o nome do aluno: ");
+//            int id = 0;
+//            boolean idValido = false;
+//
+//            while (!idValido) {
+//                try {
+//                    id = Integer.parseInt(JOptionPane.showInputDialog("Digite o Id do aluno: "));
+//                    idValido = true;
+//                } catch (NumberFormatException e) {
+//                    JOptionPane.showMessageDialog(null, "O ID deve ser numérico. Tente novamente.");
+//                }
+//            }
+//
+//            String email = JOptionPane.showInputDialog("Digite o email do estudante:");
+//
+//            if (nome.equals("sair")) {
+//                continuar = false;
+//            } else if (patternEmail.matcher(email).matches()) {
+//                cadastrarAluno(nome, id, email);
+//            } else {
+//                JOptionPane.showMessageDialog(null, "O email digitado não é válido. Tente novamente.");
+//            }
+//        }
+//
+//        System.out.println("Alunos cadastrados:");
+//        for (String aluno : alunos ) {
+//            System.out.println(aluno);
+//        }
+//
+//        //scanner.close();
+//    }
+//
+//    public static void cadastrarAluno(String nome, int id, String email) {
+//
+//        if (nome.matches("^[A-Za-z]+$")) {
+//            alunos.add("Nome: " + nome + ", ID: " + id + ", email: " + email);
+//            System.out.println("Aluno " + nome + " cadastrado com sucesso!");
+//        } else {
+//            JOptionPane.showMessageDialog(null, "O nome deve conter apenas letras. Tente novamente.");
+//        }
 
     public static void main(String[] args) {
+        ArrayList<Escola> cadastro = new ArrayList<>();
 
-        //Scanner scanner = new Scanner(System.in);
         boolean continuar = true;
 
         while (continuar) {
+            String opcao = JOptionPane.showInputDialog(null, "Escolha o que você deseja fazer:\n \n [1] Cadastrar usuário \n" +
+                    " [2] Buscar usuários \n " + "[3] Alterar o cadastro " + "\n [4] Excluir o cadastro", "Cadastro", JOptionPane.YES_NO_CANCEL_OPTION);
 
-            String nome = JOptionPane.showInputDialog("Digite o nome do aluno ou 'sair' para encerrar:");
+
+            switch (opcao) {
+                case "1":{
+                    String usuario = JOptionPane.showInputDialog("Digite seu usuário:");
+                    if (!usuario.matches("^[A-Za-z]+$")) {
+                        JOptionPane.showMessageDialog(null, "O nome deve conter apenas letras. Tente novamente.");
+                        JOptionPane.showInputDialog("Digite seu usuário:");
+                    }
+
+                    String email = JOptionPane.showInputDialog("Digite seu email:");
+
+                    String id = JOptionPane.showInputDialog("Digite seu id:");
+
+                    Escola u = new Escola();
+                    u.usuario = usuario;
+                    u.email = email;
+                    u.id = id;
+
+                    System.out.println("O usuário é:" + u.usuario);
+
+                    cadastro.add(u);
+                    break;
+                }
 
 
-            if (nome.equals("sair")) {
-
-                continuar = false;
-            } else {
-                cadastrarAluno(nome);
             }
+
+
         }
 
-        System.out.println("Alunos cadastrados:");
-        for (String aluno : alunos ) {
-            System.out.println(aluno);
-        }
-
-        //scanner.close();
-    }
-
-    public static void cadastrarAluno(String nome) {
-        if (nome.matches("^[A-Za-z]+$")) {
-            alunos.add(nome);
-            System.out.println("Aluno " + nome + " cadastrado com sucesso!");
-        }
-
-        else{
-                JOptionPane.showMessageDialog(null, "invalido");
-            }
 
     }
 }
